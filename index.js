e5cf64ea58b0c495c5c0eeb9ee70d76ce26323c9
Jessica Tovar <jessica_tovar_s@hotmail.com>
2020-02-03 19:28:48 -0600

    cambios index

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
-app.use(multer()).single('image');	//Aqui es donde se subira la imagen
+app.use(multer());	//Aqui es donde se subira la imagen
 
 // Global Variables
 app.use((req, res, next) => 
