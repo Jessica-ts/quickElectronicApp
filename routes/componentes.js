232d6c7868ea70477c845fe1e00466c46b4295c3
Jessica Tovar <jessica_tovar_s@hotmail.com>
2021-05-14 13:45:24 -0500

    Editar componente

--- routes/componentes.js
+++ routes/componentes.js
@@ -124,7 +124,7 @@ router.get('/componentes/editar/:id',isAuthenticated, async (req, res) => {
 	res.render('/componentes/editar-componente', {componente});
 });
 
-router.put('/componentes/editar-componente/:id', isAuthenticated, async (req,res) => 
+router.put('/componentes/editar-componente/', isAuthenticated, async (req,res) => 
 {
 	const {nombre, descripcion} = req.body;
 	await Componente.findOneAndUpdate(req.params.id, {nombre, descripcion});
